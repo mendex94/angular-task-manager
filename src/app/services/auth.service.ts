01eb9858460a586e143e9b8cd39f9e8bd4d31d20
Felipe Mendes <lfelipecmendes@icloud.com>
2025-02-27 09:59:54 -0300

    add login method on auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { User } from '../models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private apiUrl = 'api/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<{ token: string }> {
+    return this.http.get<User[]>(this.apiUrl).pipe(
+      map((users) => {
+        const user = users.find(
+          (u) => u.email === email && u.password === password
+        );
+
+        if (user) {
+          localStorage.setItem('token', user.token);
+          return { token: user.token };
+        } else {
+          throw new Error('Invalid credentials');
+        }
+      })
+    );
+  }
 }
